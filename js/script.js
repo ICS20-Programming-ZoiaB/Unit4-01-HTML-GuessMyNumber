506386642dc63e11167f69a772ca2c9607a1494a
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-04-17 05:13:15 +0000

    Fixed JS comment indentation

--- js/script.js
+++ js/script.js
@@ -10,12 +10,12 @@ function guessSent() {
  let correctNumber = Math.floor(Math.random() * MAX + MIN); 
  let guessedNumber = document.getElementById("user-guess").value;
 
- //if/then statement: if correctNumber is equal to guessedNumber
+//if/then statement: if correctNumber is equal to guessedNumber
  if (correctNumber == guessedNumber) {
    document.getElementById("results").innerHTML = "That is correct! Good job!";
  }
 
-  //if/then statement: if correctNumber is not equal to guessedNumber
+//if/then statement: if correctNumber is not equal to guessedNumber
 if (correctNumber != guessedNumber) {
    document.getElementById("results").innerHTML = "Your guess was not correct! My number was " + correctNumber + ".";
  }
